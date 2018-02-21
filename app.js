4b662f62f9cf1161fa19062d4d1299a7f684d1eb
Leonardo Maldonado <leonardomso11@gmail.com>
2018-02-21 13:51:30 -0300

    UI Controller starting some stuffs...

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
-console.log('Show');
+
+var budgetController = (function() {
+
+
+
+})();
+
+var UIController = (function() {
+
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector('.add__type').value,
+                description: document.querySelector('.add__description').value,
+                value: document.querySelector('.add__value').value
+            };
+        }
+    };
+
+})();
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+    const ctrlAddItem = () => {
+
+        // 1. Get the filed input data.
+        const input = UICtrl.getInput();
+        console.log(input);
+
+        // 2. Add the item to the budget controller.
+
+        // 3. Add the item to the UI.
+
+        // 4. Calculate the budget.
+
+        // 5. Display the budget on the UI.
+
+        console.log('It works.');
+    };
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+
+        if(event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+
+    });
+
+})(budgetController, UIController);
